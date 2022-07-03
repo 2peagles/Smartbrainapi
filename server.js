092cd6a33334aafe3dd821132d2a65dc70a898c1
preciousdbey <preciousdbey@penguin.lxd>
2022-07-03 05:50:16 -0400

    so confused

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-// const bodyPaser = require ('body-parser');
+const express = require( 'express' );
+// const bodyParser = require ('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
@@ -20,17 +20,17 @@ const db = knex({
     }
   });
 
-const app = express ( ) ;
+const app = express();
 
-app.use(cors( ))
+app.use(cors())
 app.use(express.json( ));
 
-app.get( '/', (req, res)=> { res.send(db.users) })
+app.get( '/',(req, res)=> { res.send(db.users) })
 app.post('/signin',  signin.handleSignin(db, bcrypt))
-app.post('./register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
-app.get('.profile/:id', (req, res) => {profile.handleProfileGet (req,res,db,bcrypt)})
-app.put('./image', (req,res) => {image.handleImage(req,res,db)})
-app.post('./imageurl', (req,res) => {image.handleApiCall (req,res)})
-app.listen ( 3000, ( )=> {
+app.post('./register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
+app.get('.profile/:id', (req, res) => { profile.handleProfileGet (req,res,db )})
+app.put('./image', (req,res) => { image.handleImage(req,res,db)})
+app.post('./imageurl', (req,res) => { image.handleApiCall (req,res)})
+app.listen(3000, ( )=> {
     console.log ('app is runnning on port 3000');
 })
