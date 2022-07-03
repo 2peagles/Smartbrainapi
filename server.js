73c679d9fea3ff5e96fdefa9ba5917660c475b02
preciousdbey <preciousdbey@penguin.lxd>
2022-07-03 07:25:40 -0400

    ..

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const db = knex({
     client: 'pg',
     connection: {
       host : '127.0.0.1', // SAME as  LOCALHOST
-      post: 3306, 
       user :'Precious',  //'your_database_user',
       password : 'Precious' , //'your_database_password',
       database :'Smart-brain' // 'myapp_test'
@@ -25,12 +24,13 @@ const app = express();
 app.use(cors())
 app.use(express.json( ));
 
-app.get( '/',(req, res)=> { res.send(db.users) })
+// app.get('/', (req,res) => {res.send('this is working')})
+// app.get( '/',(req, res)=> { res.send(db.users) })
 app.post('/signin',  signin.handleSignin(db, bcrypt))
-app.post('./register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
-app.get('.profile/:id', (req, res) => { profile.handleProfileGet (req,res,db )})
-app.put('./image', (req,res) => { image.handleImage(req,res,db)})
-app.post('./imageurl', (req,res) => { image.handleApiCall (req,res)})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet (req,res,db )})
+app.put('/image', (req,res) => { image.handleImage(req,res,db)})
+app.post('/imageurl', (req,res) => { image.handleApiCall (req,res)})
 app.listen(3000, ( )=> {
     console.log ('app is runnning on port 3000');
 })
